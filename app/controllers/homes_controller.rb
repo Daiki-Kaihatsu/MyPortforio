0ebeffcbb16d0df1320737e78d4c7fd13097db59
Daichan <daichan.kaihatsu@gmail.com>
2020-05-29 06:43:24 +0000

    ログイン機能を追加。ゲストログインも可能

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,11 @@
+class HomesController < ApplicationController
+  def index
+  end
+  
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+  end
+  
+end
